011423bc475a04fd85b9ef7af494a067618d177b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-03 01:13:58 +0000

    Automatically backed up by Learn

--- db/migrate/20191003011319_create_preferences.rb
+++ db/migrate/20191003011319_create_preferences.rb
@@ -1,6 +1,10 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
+      t.string :artist_sort_order
+      t.string :song_sort_order
+      t.boolean :allow_create_songs
+      t.boolean :allow_create_artists
     end
   end
 end
