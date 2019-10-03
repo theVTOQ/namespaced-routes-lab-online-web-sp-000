054c4a9c9f3ff51a5bd1405af10b3790d68864fe
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-03 01:06:14 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,5 +1,5 @@
 class Preference
-  attr_accessor :artist_sort_order, :song_sort_order, :allow_create_songs, :allow_create_artists
+  attr_accessor :artist_sort_order, :song_sort_order, :allow_create_songs, :allow_create_artists)
   def initialize(:artist_sort_order, :song_sort_order, :allow_create_songs, :allow_create_artists)
     @artist_sort_order = artist_sort_order
     @song_sort_order = song_sort_order
