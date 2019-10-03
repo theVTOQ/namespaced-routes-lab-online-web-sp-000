b87988724c18cc20472144ddc0d4765e25238dac
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-03 00:49:41 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,8 @@
 class Preference
   def initialize(:artist_sort_order, :song_sort_order, :allow_create_songs, :allow_create_artists)
+    @artist_sort_order = artist_sort_order
+    @song_sort_order = song_sort_order
+    @allow_create_songs = allow_create_songs
+    @allow_create_artists = allow_create_artists
+  end
 end
